c2fc4fc39719bc60ed008c571150a4ad43a081d7
joseph_dev <joseph.schneider0105@gmail.com>
2023-01-19 18:18:32 -0500

    code non maintenanble, chercher comment le rendre plus lisible

---
+++ js/api.js
@@ -0,0 +1,16 @@
+function getArticleId() {
+  return new URL(location.href).searchParams.get("id");
+}
+
+function getArticle(articleId) {
+  return fetch(`http://localhost:3000/api/products/${articleId}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((articles) => {
+      return articles;
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
