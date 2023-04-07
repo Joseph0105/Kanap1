e7320a8d667441741003d72a3b0ae210d92bcbb5
joseph_dev <joseph.schneider0105@gmail.com>
2023-04-07 12:18:35 -0400

    style de l'index

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
   }
 })();
 function getArticles() {
-  return fetch("http://localhost:3000/api/products")
+  return fetch('http://localhost:3000/api/products')
     .then((response) => {
       return response.json();
     })
@@ -20,15 +20,15 @@ function getArticles() {
 
 function displayArticles(article) {
   console.log(article);
-  const templateElt = document.getElementById("templateArticle");
+  const templateElt = document.getElementById('templateArticle');
   const cloneElt = document.importNode(templateElt.content, true);
-  const imgElt = cloneElt.querySelector("#img");
+  const imgElt = cloneElt.querySelector('#img');
 
   imgElt.src = article.imageUrl;
-  cloneElt.querySelector("#productName").textContent = article.name;
-  cloneElt.querySelector("#productPrice").textContent = article.price + " €";
-  cloneElt.getElementById("productDescription").textContent =
+  cloneElt.querySelector('#productName').textContent = article.name;
+  cloneElt.querySelector('#productPrice').textContent = article.price + '€';
+  cloneElt.getElementById('productDescription').textContent =
     article.description;
-  cloneElt.getElementById("link").href += `?id=${article._id}`;
-  document.getElementById("items").appendChild(cloneElt);
+  cloneElt.getElementById('link').href += `?id=${article._id}`;
+  document.getElementById('items').appendChild(cloneElt);
 }
